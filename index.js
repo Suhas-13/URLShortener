6e7769d3039cb34ab79fc5055b9d1fcf99bb0d7f
Suhas Hariharan <hariharan774531@sas.edu.sg>
2020-06-24 10:37:43 +0800

    -sg

--- index.js
+++ index.js
@@ -41,28 +41,13 @@ promise = mongoose.connect(connectionString, {
     useMongoClient: true
 });
 
-// Reset the app to default values when starting the server
-//
-// WARNING: Do this only when you want a fresh instance of the application else
-// comment the code.
+
 /*
 promise.then(function(db) {
     console.log('APP: Connected to MongoDB');
     URL.remove({}, function() {
         console.log('APP: URL collection emptied');
     })
-    Counter.remove({}, function() {
-        console.log('APP: Counter collection emptied');
-        console.log('APP: Initializing Counter collection with a default value');
-        var counter = new Counter({_id: 'url_count', count: 10000});
-        counter.save(function(err) {
-            if(err) {
-                console.error('APP: Error while initializing counter');
-                return console.error(err);
-            }
-            console.log('APP: Counter has been initialized');
-        });
-    });
 });
 */
 
